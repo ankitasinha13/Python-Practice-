40197e426ec1ff4d35ea26bcdad352f865ce966a
Ankita Sinha <sinhaankita250@gmail.com>
2026-07-19 21:00:32 +0530

    Add function to find the largest of three numbers

---
+++ largest_of_three_numbers.py
@@ -0,0 +1,9 @@
+a = int(input("Enter 1st no:"))
+b = int(input("Enter 2nd no:"))
+c = int(input("Enter 3rd no:"))
+if a > b and a > c :
+    print("The largest no is:",a)
+elif b > a and b > c :
+    print("The largest no is:",b)
+else :
+    print("The largest no is:",c)
