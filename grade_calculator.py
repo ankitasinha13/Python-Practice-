075f93106036fd1d73f8f7d4b1109f05c23d2573
Ankita Sinha <sinhaankita250@gmail.com>
2026-07-20 20:33:49 +0530

    Add grade calculator based on marks input

---
+++ grade_calculator.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the marks:"))
+if a <= 100 and a >= 90:
+    print("The grade obtained A")
+elif a <= 89 and a >= 80:
+    print("The grade obtained B")
+elif a <= 79 and a >= 70:
+    print("The grade obtained C")
+elif a <= 69 and a >= 60:
+    print("The grade obtained D")
+else:
+    print("The grade obtained F")
