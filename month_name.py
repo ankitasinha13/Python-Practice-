3a1d5fd50aee7e5f5cb54ef2f5314f3563a0d302
Ankita Sinha <sinhaankita250@gmail.com>
2026-07-20 20:35:08 +0530

    Add month name printing based on user input

---
+++ month_name.py
@@ -0,0 +1,27 @@
+x = int(input("Enter a month number:"))
+if x == 1:
+    print("The month is january")
+elif x == 2:
+    print("The month is february")
+elif x == 3:
+    print("The month is march")
+elif x == 4:
+    print("The month is april")
+elif x == 5:
+    print("The month is may")
+elif x == 6:
+    print("The month is june")
+elif x == 7:
+    print("The month is july")
+elif x == 8:
+    print("The month is august")
+elif x == 9:
+    print("The month is september")
+elif x == 10:
+    print("The month is october")
+elif x == 11:
+    print("The month is november")
+elif x == 12:
+    print("The month is december")
+else:
+    print("Invalid month number")
