a6c7f137ea5fe56bb66fa7e98a1a3f0a66a59ce8
Ankita Sinha <yourgithubemail@gmail.com>
2026-08-04 19:38:18 +0530

    pyramid question

---
+++ Star_pattern.py
@@ -0,0 +1,18 @@
+#left aligned pyramid
+"""for i in range(1,6):
+    for j in range(i):
+        print("*",end="")
+    print()
+    """
+
+#mid pyramid
+"""for i in range(1,6):
+   print(" "*(5-i),end="")
+   print("*"*(2*i-1))
+print()
+"""
+#right aligned pyramid
+for i in range(1,6):
+   print(" "*(5-i),end="")
+   print("*"*i)
+print()
