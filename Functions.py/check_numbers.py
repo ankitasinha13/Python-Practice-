e7df8fea824db32f152008e75c24bad054631575
Ankita Sinha <yourgithubemail@gmail.com>
2026-08-06 11:59:40 +0530

    check no which is zero,pos,neg using function

---
+++ Functions.py/check_numbers.py
@@ -0,0 +1,10 @@
+def check_numbers(number):
+    if(number>0):
+        print(number,"is positive")
+    elif(number<0):
+        print(number,"is negative")
+    else:
+        print(number,"is zero")
+num = check_numbers(45)
+num = check_numbers(-80)
+num = check_numbers(0)
